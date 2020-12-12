9d3c02c72610591134e0b929ab078a7cf4c62997
Krishna <72448053+KrishnaVedampudi@users.noreply.github.com>
2020-12-12 12:40:26 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 faded = "";
 hptheme = "";
+leftWristX = 0;
+rightWristX = 0;
+leftWristY = 0;
+rightWristY = 0;
 function preload()
 {
    faded = loadSound('faded.mp3');
@@ -12,9 +16,26 @@ function setup()
     canvas.center();
 
     video = createCapture(VIDEO);
-    video.hide();                   
+    video.hide();
+    
+     poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+function gotPoses(results)
+{
+    if(results.length > 0)
+    {          
+        console.log(results);        
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("Left Wrist X = "+leftWristX+"Left Wrist Y = "+leftWristY);
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;        
+        console.log("Right Wrist X = "+rightWristX+"Right Wrist Y = "+rightWristY);
+    }    
 }
 function draw()
 {
     image(video, 0, 0, 500, 500);
 }
+
